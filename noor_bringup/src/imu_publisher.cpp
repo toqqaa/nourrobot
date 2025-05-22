231b5d5ae991ce34ff2842d846531e8bf953de0d
toqqaa <https://github.com/toqqaa>
2025-05-22 10:55:18 +0300

    updated

--- noor_bringup/src/imu_publisher.cpp
+++ noor_bringup/src/imu_publisher.cpp
@@ -1,31 +1,3 @@
-//=================================================================================================
-// Copyright (c) 2014, Christian Rose, TU Darmstadt
-// All rights reserved.
-
-// Redistribution and use in source and binary forms, with or without
-// modification, are permitted provided that the following conditions are met:
-//     * Redistributions of source code must retain the above copyright
-//       notice, this list of conditions and the following disclaimer.
-//     * Redistributions in binary form must reproduce the above copyright
-//       notice, this list of conditions and the following disclaimer in the
-//       documentation and/or other materials provided with the distribution.
-//     * Neither the name of the Simulation, Systems Optimization and Robotics
-//       group, TU Darmstadt nor the names of its contributors may be used to
-//       endorse or promote products derived from this software without
-//       specific prior written permission.
-
-// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-// ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER BE LIABLE FOR ANY
-// DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-// ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-//=================================================================================================
-
 #include <ros/ros.h>
 #include <geometry_msgs/Quaternion.h>
 #include <geometry_msgs/PoseStamped.h>
@@ -35,80 +7,96 @@ ros::Publisher imuPub_;
 ros::Publisher posePub_;
 
 geometry_msgs::PoseStamped pose_msg;
-
 sensor_msgs::Imu imu_msg;
 
-void quaternionCb(const geometry_msgs::Quaternion::ConstPtr& msg){
+void quaternionCb(const geometry_msgs::Quaternion::ConstPtr& msg)
+{
+    bool pose_subscribed = (posePub_.getNumSubscribers() > 0);
+    bool imu_subscribed = (imuPub_.getNumSubscribers() > 0);
 
-  bool pose_subscribed = (posePub_.getNumSubscribers() > 0);
-  bool imu_subscribed  = (imuPub_.getNumSubscribers() > 0);
+    if (pose_subscribed || imu_subscribed)
+    {
+        ros::Time sensor_data_time = ros::Time::now(); // Removed time offset for reliability
 
-  if (pose_subscribed || imu_subscribed){
+        if (imu_subscribed)
+        {
+            // Fill IMU message
+            imu_msg.header.stamp = sensor_data_time;
+            imu_msg.orientation = *msg;
 
-    //@TODO make time offset param
-    ros::Time sensor_data_time = ros::Time::now() - ros::Duration(0.002);
+            // Required fields for Madgwick filter
+            imu_msg.angular_velocity.x = 0.0;  // Required - set to 0 if no gyro data
+            imu_msg.angular_velocity.y = 0.0;
+            imu_msg.angular_velocity.z = 0.0;
 
-    if (imu_subscribed){
-      imu_msg.header.stamp = sensor_data_time;
+            imu_msg.linear_acceleration.x = 0.0;  // Recommended
+            imu_msg.linear_acceleration.y = 0.0;
+            imu_msg.linear_acceleration.z = 9.81; // Default gravity value
 
-      imu_msg.orientation = *msg;
+            imuPub_.publish(imu_msg);
+        }
 
-      imuPub_.publish(imu_msg);
+        if (pose_subscribed)
+        {
+            pose_msg.header.stamp = sensor_data_time;
+            pose_msg.pose.orientation = *msg;
+            posePub_.publish(pose_msg);
+        }
     }
-
-    if (pose_subscribed){
-      pose_msg.header.stamp = sensor_data_time;
-
-      pose_msg.pose.orientation = *msg;
-
-      posePub_.publish(pose_msg);
-    }
-  }
 }
 
-int main(int argc, char** argv){
-
+int main(int argc, char** argv)
+{
     ros::init(argc, argv, "mpu6050_imu_converter");
+    ros::NodeHandle nh;
+    ros::NodeHandle pnh("~");
 
-    ros::NodeHandle _nh;
-    ros::NodeHandle _pnh("~");
-
+    // Parameters
     double linear_acceleration_stdev, angular_velocity_stdev;
     std::string frame_id;
-    std::string frame_id2;
 
-    ros::param::param<double>("~linear_acceleration_stdev", linear_acceleration_stdev, 0.06);
-    ros::param::param<double>("~angular_velocity_stdev", angular_velocity_stdev, 0.005);
-    ros::param::param<std::string>("~frame_id", frame_id, std::string("imu_link"));
+    pnh.param<double>("linear_acceleration_stdev", linear_acceleration_stdev, 0.06);
+    pnh.param<double>("angular_velocity_stdev", angular_velocity_stdev, 0.005);
+    pnh.param<std::string>("frame_id", frame_id, "imu_link");
 
+    // Convert stdev to variance for covariance matrices
     double linear_acceleration_cov = linear_acceleration_stdev * linear_acceleration_stdev;
     double angular_velocity_cov = angular_velocity_stdev * angular_velocity_stdev;
 
-
+    // Setup IMU message
     imu_msg.header.frame_id = frame_id;
     pose_msg.header.frame_id = frame_id;
 
-    imu_msg.orientation_covariance[0] = 0.1;
-    imu_msg.orientation_covariance[4] = 0.1;
-    imu_msg.orientation_covariance[8] = 0.1;
+    // Orientation covariance (3x3 row-major)
+    imu_msg.orientation_covariance = {
+        0.1, 0.0, 0.0,
+        0.0, 0.1, 0.0,
+        0.0, 0.0, 0.1
+    };
 
+    // Angular velocity covariance (3x3 row-major)
+    imu_msg.angular_velocity_covariance = {
+        angular_velocity_cov, 0.0, 0.0,
+        0.0, angular_velocity_cov, 0.0,
+        0.0, 0.0, angular_velocity_cov
+    };
 
-    // Angular velocity entries are not filled, so set to -1.0
-    imu_msg.angular_velocity_covariance[0] = 0.0; //angular_velocity_cov;
-    imu_msg.angular_velocity_covariance[4] = angular_velocity_cov;
-    imu_msg.angular_velocity_covariance[8] = angular_velocity_cov;
+    // Linear acceleration covariance (3x3 row-major)
+    imu_msg.linear_acceleration_covariance = {
+        linear_acceleration_cov, 0.0, 0.0,
+        0.0, linear_acceleration_cov, 0.0,
+        0.0, 0.0, linear_acceleration_cov
+    };
 
-    // Linear acceleration entries are not filled, so set to -1.0
-    imu_msg.linear_acceleration_covariance[0] = 0.0;
-    imu_msg.linear_acceleration_covariance[4] = linear_acceleration_cov;
-    imu_msg.linear_acceleration_covariance[8] = linear_acceleration_cov;
+    // Publishers
+    posePub_ = pnh.advertise<geometry_msgs::PoseStamped>("pose", 10);
+    imuPub_ = pnh.advertise<sensor_msgs::Imu>("/imu/data_raw", 10); // Relative topic name
 
-    posePub_ = _pnh.advertise<geometry_msgs::PoseStamped>("pose", 10);
-    imuPub_ = _pnh.advertise<sensor_msgs::Imu>("/imu", 10);
+    // Subscriber
+    ros::Subscriber quatSub = nh.subscribe("quaternion", 10, quaternionCb);
 
-    ros::Subscriber quatSub_ = _nh.subscribe("quaternion", 10, quaternionCb);
-
+    ROS_INFO("MPU6050 IMU Converter node started");
     ros::spin();
 
-    return(0);
+    return 0;
 }
